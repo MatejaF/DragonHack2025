0b84d42341df3e919889361cd7485e549c5771ad
nezazp <nz8762@student.uni-lj.si>
2025-04-12 21:26:13 +0200

    vprašalniki

--- app/src/main/java/com/example/medimate/MainActivity2.java
+++ app/src/main/java/com/example/medimate/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.example.medimate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,16 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
+        Button clicker = (Button) findViewById(R.id.btnContinueQ2);
+
+        clicker.setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v){
+               Intent intent = new Intent(MainActivity2.this, secondQ.class);
+               startActivity(intent);
+           }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
