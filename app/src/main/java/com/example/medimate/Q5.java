8675f371ff83c62e5c2ee41f09499c22e6ad526e
Mateja <146631515+MatejaF@users.noreply.github.com>
2025-04-13 03:01:08 +0200

    Calendar zadnji page

--- app/src/main/java/com/example/medimate/Q5.java
+++ app/src/main/java/com/example/medimate/Q5.java
@@ -1,6 +1,8 @@
 package com.example.medimate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,12 @@ public class Q5 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_q5);
+
+        Button clicker = (Button) findViewById(R.id.btnContinueQ6);
+        clicker.setOnClickListener(v -> {
+            Intent intent = new Intent(Q5.this, Calendar.class);
+            startActivity(intent);
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.q5), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
